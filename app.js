9af970aa1a74e879fe2ec97b335cb7051ba9c7bc
Munachimso <Victorycollins31@gmail.com>
2023-12-13 21:53:13 +0100

    Update: set up start function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express(); 
 
-const notFound = require('./middleware/not-found')
+const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
 //express json middleware
@@ -12,5 +12,23 @@ app.use(express.json())
 
 // routees 
 app.get('/',(req, res)=>{
-    res.send('<h1>Store Api</h1><a href="/api/v1/products"')
+    res.send('<h1>Store Api</h1><a href="/api/v1/products">products route</a>')
 })
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () =>{
+    try {
+        app.listen(port, console.log(`server is listening to port ${port}`))
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+start()
