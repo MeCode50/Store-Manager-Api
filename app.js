6c3993f671bde77b0c5a50fe792a7793602530c4
Munachimso <Victorycollins31@gmail.com>
2023-12-13 21:36:17 +0100

    New features: set up  routes, link and

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('04 Store API')
+require('dotenv').config()
+
+
+const express = require('express');
+const app = express(); 
+
+const notFound = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+//express json middleware
+app.use(express.json())
+
+// routees 
+app.get('/',(req, res)=>{
+    res.send('<h1>Store Api</h1><a href="/api/v1/products"')
+})
