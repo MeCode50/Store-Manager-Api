0a93086b977d9ef9166a21b210f68f9a7826a1f7
Munachimso <Victorycollins31@gmail.com>
2023-12-14 22:25:33 +0100

    Fix: database connection

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 const express = require('express');
 const app = express(); 
+const connectDB = require('./db/connect')
+
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -24,6 +26,7 @@ const port = process.env.PORT || 3000
 
 const start = async () =>{
     try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, console.log(`server is listening to port ${port}`))
     } catch (error) {
         console.log(error)
